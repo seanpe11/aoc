64aa5a8c457b17a209b4ae804ab43bc24a0e9bd9
Sean Pe <seanpe@pop-os.localdomain>
2022-12-07 02:25:50 +0800

    working on recovering

---
+++ 2022/day6/part1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <map>
+#include <stack>
+#include <stdio.h>
+#include <vector>
+using namespace std;
+
+// simple stack solution
+// read input into stacks, then just follow operations as stacks
+
+int main(void) {
+  string line;
+  getline(cin, line);
+
+  return 0;
+}
