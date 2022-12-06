eb61df601bed7a4c51d46c5589b8f70ea33996f5
Sean Pe <seanpe@pop-os.localdomain>
2022-12-07 02:35:54 +0800

    day6 2022 fixed

--- 2022/day6/part1.cpp
+++ 2022/day6/part1.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <map>
+#include <set>
 #include <stack>
 #include <stdio.h>
 #include <vector>
@@ -11,6 +11,21 @@ using namespace std;
 int main(void) {
   string line;
   getline(cin, line);
+  int ans;
+  int length = 4;
+  // int length = 14;
+
+  for (int i = 0; i < line.length() - length; i++) {
+    set<char> s;
+    for (int j = 0; j < length; j++) {
+      s.insert(line[i + j]);
+    }
+    if (s.size() == length) {
+      ans = i + length;
+      break;
+    }
+  }
+  cout << ans;
 
   return 0;
 }
