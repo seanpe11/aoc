26bd2be8e41de701e5ea51372816ac21be565d4c
seanpe11 <sean.m.s.pe@gmail.com>
2023-12-29 22:23:41 +0800

    finished day1-2023p2

--- 2023/day1/day1-2023.py
+++ 2023/day1/day1-2023.py
@@ -1,5 +1,8 @@
 sum = 0
 
+# part 1
+
+print("Part 1:")
 for line in open("input.txt", "r"):
     left = 0
     right = len(line) - 1
@@ -7,9 +10,45 @@ for line in open("input.txt", "r"):
         left+=1
     while (not line[right].isdigit()):
         right-=1
-
-    print(f"{line[left]} {line[right]}")
-
     sum+= int(line[left] + line[right])
+print(sum)
+
+
+# part 2
+
+print("\n\nPart 2:")
+# zirk to pad it so i can use index
+numbers = ["&", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+sum = 0
+for line in open("input.txt", "r"):
+    left = 0
+    right = len(line) - 1
+    start = ""
+    end = ""
+
+    while(start==""):
+        if (line[left].isdigit()):
+            start = line[left]
+        else:
+            for n in numbers:
+                # print(line[left:len(n)+left])
+                if line[left:len(n)+left] == n:
+                    start = str(numbers.index(line[left:len(n)+left]))
+                    break
+        left += 1
+    while(end==""):
+        if (line[right].isdigit()):
+            end = line[right]
+        else:
+            for n in numbers:
+                # print(line[left:len(n)+left])
+                if line[right-len(n):right] == n:
+                    end = str(numbers.index(line[right-len(n):right]))
+                    break
+        right -= 1
+
+    print(start+end)
+    sum += int(start + end)
+
 
 print(sum)
